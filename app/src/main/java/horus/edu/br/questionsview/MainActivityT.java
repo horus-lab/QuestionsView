2619b823a105ac1f930cf450a5b8228cc992e346
Rodrigo Bernardi <coloradonanet@hotmai.com>
2016-08-21 10:56:18 -0300

    Initial commit

---
+++ app/src/main/java/horus/edu/br/questionsview/MainActivityT.java
@@ -0,0 +1,13 @@
+package horus.edu.br.questionsview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_t);
+    }
+}
